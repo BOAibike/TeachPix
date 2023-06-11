b47576fb9c3be4cea1fcc541f3bfb2d0a21c4abe
Aibike Bektursynova <ainikojuice@gmail.com>
2023-06-11 19:55:47 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const generateBlock = document.getElementById('generate-block');
+const generateBtn = document.getElementById('generate-btn');
+
+generateBtn.addEventListener('click', function(){
+    generateBlock.style.display = 'block';
+}) 
+
