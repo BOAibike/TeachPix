f334f7ed2ea02420b69ff6d718cf801376df6d15
Aibike Bektursynova <ainikojuice@gmail.com>
2023-06-12 02:13:56 +0600

    generator bar

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+// Opens a generate section
 const generateBlock = document.getElementById('generate-block');
 const generateBtn = document.getElementById('generate-btn');
+
 
 generateBtn.addEventListener('click', function(){
     generateBlock.style.display = 'block';
 }) 
 
+// Changes 'close'-button
